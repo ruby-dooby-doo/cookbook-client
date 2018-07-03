e4c85a9343afabc6f82789161897631064f65c14
Brian <riceb53@gmail.com>
2018-07-03 18:34:46 -0500

    make new with HTML form that doesn't work, and make create that should work

--- app/controllers/client/recipes_controller.rb
+++ app/controllers/client/recipes_controller.rb
@@ -15,4 +15,22 @@ class Client::RecipesController < ApplicationController
   def new
     render "new.html.erb"
   end
+
+  def create
+    # take in the parameters
+    # make a web request to the API to create the new thing
+    response = Unirest.post("localhost:3000/api/recipes", parameters:
+      {
+        input_title: params[:input_title],
+        input_chef: params[:input_chef],
+        input_directions: params[:input_directions],
+        input_prep_time: params[:input_prep_time],
+        input_ingredients: params[:input_ingredients],
+        input_image_url: params[:input_image_url]
+      }
+    )
+    @recipe = response.body
+    # show the newly created recipe to the user in HTML
+    render "show.html.erb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
     get '/recipes' => 'recipes#index'
     get '/recipes/new' => 'recipes#new'
     get '/recipes/:id' => 'recipes#show'
+    post '/recipes' => 'recipes#create'
   end
 end
