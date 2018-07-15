58133ef928fc66a5583ca09789bdaa6f7c358d0b
Brian <riceb53@gmail.com>
2018-07-15 11:29:40 -0500

    replace chef with user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # STEP 1: A ROUTE triggers a controller action
   # verb "/urls" => "namespace/controllers#action"
+  get '/' => 'client/recipes#index'
   namespace :client do
     get '/recipes' => 'recipes#index'
     get '/recipes/new' => 'recipes#new'
