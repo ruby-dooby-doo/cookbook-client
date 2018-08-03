e8836cdc4cf8b56166cb8c9c4bcddf8d1bd84449
Brian <riceb53@gmail.com>
2018-08-02 20:16:43 -0500

    add index.html

---
+++ public/index.js
@@ -0,0 +1,14 @@
+console.log('in index.js');
+
+// get the box
+// get the thing you want to put in the box
+// copy the thing and put it in the box
+
+var recipeTemplate = document.querySelector('#recipe-card');
+
+var recipeContainer = document.querySelector('.row');
+
+recipeContainer.appendChild(recipeTemplate.content.cloneNode(true));
+recipeContainer.appendChild(recipeTemplate.content.cloneNode(true));
+recipeContainer.appendChild(recipeTemplate.content.cloneNode(true));
+recipeContainer.appendChild(recipeTemplate.content.cloneNode(true));
